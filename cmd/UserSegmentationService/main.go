47fbc6f430c42be24742aca1fb26ad9c8ec217cc
Gurshumov Daniil Bakhtiyarovich <daniil.gurshumov@yandex.ru>
2025-07-11 17:24:17 +0300

    test commit

---
+++ cmd/UserSegmentationService/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":8080", r)
+}
